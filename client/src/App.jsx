603283d9042ca43d08480165f6c4e0444dba537c
anurag-12042002 <anuragjaiswal331@gmail.com>
2026-03-31 20:09:06 +0530

    pathway

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div>
-      <Navbar />
-      </div>
+    <>
+      {!isOwnerPath &&<Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+
+      </>
   )
 }
 export default App
