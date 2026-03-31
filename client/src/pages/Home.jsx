47402a74d115e84caa2635bd30e62033a410cc2f
anurag-12042002 <anuragjaiswal331@gmail.com>
2026-04-01 05:25:05 +0530

    Banner and Testimonial feature are to be added.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
+      <Testimonial />
     </>
   )
 }
