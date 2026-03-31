b2cfef5f7e82dca6adb250041e2eaf0e5280e50b
anurag-12042002 <anuragjaiswal331@gmail.com>
2026-03-31 23:05:26 +0530

    Homescreen booking date and return date with car image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Car'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
 
@@ -10,13 +13,16 @@ const App = () => {
 
   return (
     <>
-      {!isOwnerPath &&<Navbar setShowLogin={setShowLogin}/>}
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
       </Routes>
 
-      </>
+    </>
   )
 }
 export default App
---
+++ client/src/components/CarCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CarCard = ({ car }) => {
+
+    const currency = import.meta.env.VITE_CURRENCY 
+
+    return (
+        <div className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1
+    transition-all duration-500 cursor-pointer'>
+
+            <div className='relative h-48 overflow-hidden'>
+                <img src={car.image} alt="Car Image" className='w-full h-full object-cover
+        transition-transform duration-500 group-hover:scale-105'/>
+
+                {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90
+        text-white text-xs px-2.5 py-1 rounded-full'>Available Now</p>}
+
+                <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm
+        text-white px-3 py-2 rounded-lg'>
+                    <span className='font-semibold'>{currency}{car.pricePerDay}</span>
+                    <span className='text-sm text-white/80'> / day</span>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default CarCard
